dfb1be4f27999c42a576822245984a3310120573
DanierJ <danierjavid@gmail.com>
2019-06-04 18:04:27 -0500

    6. type casting due to the default convertion of java to int

--- ByteShortInt/src/com/danjerous/Main.java
+++ ByteShortInt/src/com/danjerous/Main.java
@@ -9,10 +9,11 @@ public class Main {
         long myLongValue = 100L; // The L is to tell that is a long value
         //  min -2*63, max 2*63
 
-
         // Int has a width of 32
         int minValue = -2_147_483_648; // This is the minimum value that it takes.
         int maxValue = 2_147_483_647; // This is the maximum value that it takes.
+        int myTotal = (minValue/2);
+        System.out.println("My Total int:" + myTotal);
 
         // Short has width of 16
         short myShortValue = 20; // a bit more larger
@@ -21,6 +22,9 @@ public class Main {
         // byte has a width of 8
         byte myByteValue = 10; // smaller amount of space
         // min -128, max 127
+        byte myByteTotal = (byte)(myByteValue/2); /// Java automatically convert expressions into integers so we need to tell the right data type (Type casting)
+        System.out.println("My total Byte:" + myByteTotal);
+
 
 
     }
