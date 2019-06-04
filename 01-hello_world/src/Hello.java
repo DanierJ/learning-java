7adcdc43c1443cbb1a700e680beaf84ddce5d4f2
DanierJ <danierjavid@gmail.com>
2019-06-04 17:21:14 -0500

    Archivos iniciales y hello world

---
+++ 01-hello_world/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main (String[] args) { //EntryPoint for our program
+
+        System.out.println("Hello, World!"); // Print something out with printline.
+    }
+
+}
