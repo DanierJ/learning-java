b1f23efe6a8b86ece9b28b4ba2281c03a2b5d9ff
DanierJ <danierjavid@gmail.com>
2019-06-04 17:35:17 -0500

    2. Uso variables tipo int

--- 01-hello_world/src/Hello.java
+++ 01-hello_world/src/Hello.java
@@ -3,6 +3,17 @@ public class Hello {
     public static void main (String[] args) { //EntryPoint for our program
 
         System.out.println("Hello, World!"); // Print something out with printline.
+
+
+        // Data type integer (int).
+        int myFirstNumber = 10 + 5;
+        int mySecondNumber = 65;
+        int myThirdNumber = 80;
+        int result = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myTenResult = result * 10;
+
+        // Printing a variable
+        System.out.println(myFirstNumber + result + myTenResult);
     }
 
 }
