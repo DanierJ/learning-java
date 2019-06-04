a5f676d8a4f5baf68c0def391913444723dc103c
DanierJ <danierjavid@gmail.com>
2019-06-04 17:52:40 -0500

    5. short data type, min and max values

--- ByteShortInt/src/com/danjerous/Main.java
+++ ByteShortInt/src/com/danjerous/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+     
+
+        // Int has a width of 32
         int minValue = -2_147_483_648; // This is the minimum value that it takes.
         int maxValue = 2_147_483_647; // This is the maximum value that it takes.
 
+        // Short has width of 16
+        short myShortValue = 20; // a bit more larger
+        // min -32768, max 32767. Twice a byte
+
+        // byte has a width of 8
         byte myByteValue = 10; // smaller amount of space
         // min -128, max 127
+
+
     }
 }
