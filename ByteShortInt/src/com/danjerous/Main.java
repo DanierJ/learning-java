a6a66675c2ee386267a3596dc71fcccba3d23603
DanierJ <danierjavid@gmail.com>
2019-06-04 17:44:43 -0500

    3. int data type, min and max values

---
+++ ByteShortInt/src/com/danjerous/Main.java
@@ -0,0 +1,10 @@
+package com.danjerous;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int minValue = -2_147_483_648; // This is the minimum value that it takes.
+        int maxValue = 2_147_483_647; // This is the maximum value that it takes.
+    }
+}
