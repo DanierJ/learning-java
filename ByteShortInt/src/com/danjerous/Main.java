dad4adce2710985d59b767a3a886c2ba765a0cc6
DanierJ <danierjavid@gmail.com>
2019-06-04 17:47:44 -0500

    4. byte data type, min and max values

--- ByteShortInt/src/com/danjerous/Main.java
+++ ByteShortInt/src/com/danjerous/Main.java
@@ -6,5 +6,8 @@ public class Main {
 
         int minValue = -2_147_483_648; // This is the minimum value that it takes.
         int maxValue = 2_147_483_647; // This is the maximum value that it takes.
+
+        byte myByteValue = 10; // smaller amount of space
+        // min -128, max 127
     }
 }
