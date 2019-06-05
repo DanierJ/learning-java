3f7f76af0456a2179449255773ab51c0fd311c54
DanierJ <danierjavid@gmail.com>
2019-06-05 15:41:35 -0500

    Challenge with methods

--- Methods/src/com/danjerous/Main.java
+++ Methods/src/com/danjerous/Main.java
@@ -7,7 +7,19 @@ public class Main {
 
         score = calculateScore(true, 100, 5, 200 );
 
+        System.out.println("Your final score was: " + score);
+
         score = calculateScore(true, 300, 8, 600 );
+
+        System.out.println("Your final score was: " + score);
+
+
+        System.out.println("SCORE TABLE");
+
+        displayHighScorePosition("Jason", calculateHighScorePosition(1500));displayHighScorePosition("Zordan", calculateHighScorePosition(900));displayHighScorePosition("Baba", calculateHighScorePosition(400));displayHighScorePosition("Craik", calculateHighScorePosition(50));
+
+
+
 
     }
 
@@ -15,10 +27,27 @@ public class Main {
 
         if (gameOver) {
             int finalScore = score + bonus;
-            System.out.println("Your final score was" + finalScore);
+
             return  finalScore;
         }
 
         return  -1;
+    }
+
+    public static void displayHighScorePosition (String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + "on the high score table");
+    }
+
+    public static int calculateHighScorePosition (int score) {
+
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500  && score < 1000) {
+            return 2;
+        } else if (score >= 100 && score < 500) {
+            return 3;
+        } else {
+            return  4;
+        }
     }
 }
