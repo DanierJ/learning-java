3f79886bc04b3bcedf9e2a55d236d0c268ddd5d5
DanierJ <danierjavid@gmail.com>
2019-06-05 17:25:43 -0500

    14. Working with while and doWhile

---
+++ WhileDoWhile/src/com/danjerous/Main.java
@@ -0,0 +1,21 @@
+package com.danjerous;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         int truty = 4;
+
+         while (truty < 4) {
+
+             System.out.println("truty");
+
+             truty++;
+
+         }
+
+         do {
+             System.out.println("truty");
+         } while (truty != 4);
+    }
+}
