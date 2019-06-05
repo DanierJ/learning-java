49d70b1b07e6420043dc387d3b4b9503c7678918
DanierJ <danierjavid@gmail.com>
2019-06-05 14:33:13 -0500

    11. Keywords and expressions

---
+++ KeywordsAndExpressions/src/com/danjerous/Main.java
@@ -0,0 +1,9 @@
+package com.danjerous;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Reserved words ---> Keywords ;
+        // Operations, conditionals, thins inside parenthesis, etc  ----> Expressions;
+    }
+}
