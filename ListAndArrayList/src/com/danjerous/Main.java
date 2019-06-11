69cc1825459a26b2ebb5ecdcd0bca5a32c01727f
DanierJ <danierjavid@gmail.com>
2019-06-11 17:13:27 -0500

    25. Working with ArrayLists

--- ListAndArrayList/src/com/danjerous/Main.java
+++ ListAndArrayList/src/com/danjerous/Main.java
@@ -1,43 +1,116 @@
 package com.danjerous;
 
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
-    private static int[] baseData = new int[10];
+    private  static Scanner scanner = new Scanner(System.in);
+    private static Grocery groceryList = new Grocery();
 
     public static void main(String[] args) {
-        System.out.println("Enter 10 integers:");
-        getInput();
-        printArray(baseData);
-        resizeArray();
-        System.out.println("Enter 12 integers:");
-        getInput();
-        printArray(baseData);
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
 
-    }
+        while(!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
-    private static void getInput () {
-        for (int i = 0; i < baseData.length; i++) {
-            baseData[i] = scanner.nextInt();
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    processArrayList();
+                    break;
+                case 7:
+                    quit = true;
+                    break;
+            }
         }
     }
 
-    private static void printArray (int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
-        }
-            System.out.println();
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To process array list.");
+        System.out.println("\t 7 - To quit the application.");
     }
 
-    private static void resizeArray () {
-        int[] original = baseData;
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
+        groceryList.addGroceryItem(scanner.nextLine());
+    }
 
-        baseData = new int[12];
+    public static void modifyItem () {
+        System.out.print("Current item name: ");
+        String itemNo = scanner.nextLine();
+        System.out.print("Enter replacement item: ");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryItem(itemNo, newItem);
+    }
 
-        for (int i = 0; i < original.length; i++) {
-            baseData[i] = original[i];
+    public static void removeItem() {
+        System.out.print("Enter item name being deleted: ");
+        String itemNo = scanner.nextLine();
+        groceryList.removeGroceryItem(itemNo);
+    }
+
+    public static void searchForItem() {
+        System.out.print("Item to search for: ");
+        String searchItem = scanner.nextLine();
+
+        if (groceryList.onFile(searchItem)) {
+            System.out.println("Found [" + searchItem + "] in your grocery list.");
+        } else {
+            System.out.println("[" + searchItem + "] is not in the shopping list.");
         }
     }
+
+    public static void processArrayList() {
+
+        /// To copy the entire array into other array.
+        ArrayList<String> newArray = new ArrayList<String>();
+        newArray.addAll(groceryList.getGroceryList());
+
+        /// Another way
+
+        ArrayList<String> nextArray = new ArrayList<>(groceryList.getGroceryList());
+
+
+        // Converting to regular array
+
+        String[] myArray = new String[groceryList.getGroceryList().size()];
+
+        myArray = groceryList.getGroceryList().toArray(myArray);
+
+
+
+    }
+
+
+
 }
