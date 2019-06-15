16be6d3c1d539b47aa50a777a1999ad89cf28dbd
DanierJ <danierjavid@gmail.com>
2019-06-15 17:13:19 -0500

    My idea for the challenge

--- LinkedListChallenge/src/com/danjerous/PlayList.java
+++ LinkedListChallenge/src/com/danjerous/PlayList.java
@@ -1,35 +1,47 @@
 package com.danjerous;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
 public class PlayList {
 
-    private static LinkedList<Song> playList;
+    private static LinkedList<Song> playList = new LinkedList<>();
+    private static ArrayList<Album> albums = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        Album mrAZ = new Album("mrAZ");
+        Album mrAZ = new Album("mrAZ", "Json Mraz");
 
-        Song song1 = new Song("I'm yours", 4.00);
-        Song song2 = new Song("Song for a friend", 5.00);
+        mrAZ.addSong("I'm yours", 4.00);
+        mrAZ.addSong("Song for a friend.", 5.00);
 
-        mrAZ.getSongList().add(song1);
-        mrAZ.getSongList().add(song2);
-
-        LinkedList<Song> playList = new LinkedList<>();
-
-        playList.add(song1);
-        playList.add(song2);
+        albums.add(mrAZ);
 
         ListIterator listIterator = playList.listIterator();
 
         while (listIterator.hasNext()) {
-            Song song = (Song)listIterator.next();
+            Song song = (Song) listIterator.next();
             System.out.println("Song: " + song.getTitle());
         }
 
 
+    }
+
+    private void addSong(String albumName, Song song) {
+        if(findAlbum(albumName) >= 0) {
+
+        }
+    }
+
+    private int findAlbum (String albumName) {
+        for (int i = 0; i < albums.size() ; i++) {
+            if(albums.get(i).getAlbumName().equals(albumName)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
