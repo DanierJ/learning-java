57e7d7d3f2e082d7341059b479e6bb148fbfac7c
DanierJ <danierjavid@gmail.com>
2019-06-20 16:36:03 -0500

    35. Collections overview

---
+++ Collections/src/com/danjerous/Main.java
@@ -0,0 +1,15 @@
+package com.danjerous;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Theatre theatre = new Theatre("Procinal", 8, 12);
+	    theatre.getSeats();
+
+	    if (theatre.reservedSeat("H11")) {
+            System.out.println("Please pay.");
+        } else {
+            System.out.println("Sorry, seat is taken.");
+        }
+    }
+}
