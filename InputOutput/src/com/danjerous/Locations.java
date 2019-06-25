f91edb7f81abcdf2e222068192107e18f187ec2c
DanierJ <danierjavid@gmail.com>
2019-06-25 18:33:35 -0500

    loading big files

--- InputOutput/src/com/danjerous/Locations.java
+++ InputOutput/src/com/danjerous/Locations.java
@@ -43,7 +43,7 @@ public class Locations implements Map<Integer, Location> {
         Scanner scanner = null;
 
         try {
-            scanner = new Scanner(new FileReader("locations.txt"));
+            scanner = new Scanner(new FileReader("locations_big.txt"));
             scanner.useDelimiter(",");
             while(scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
@@ -64,7 +64,7 @@ public class Locations implements Map<Integer, Location> {
         // Now read the exists
 
         try {
-          scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+          scanner = new Scanner(new BufferedReader(new FileReader("directions_big.txt")));
 
           scanner.useDelimiter(",");
 
