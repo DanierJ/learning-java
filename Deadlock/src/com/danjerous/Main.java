3f8db34e819a7197eba6a012112ef4238d4a26c7
DanierJ <danierjavid@gmail.com>
2019-06-28 17:35:01 -0500

    38.9 Thread issues: Deadlock

---
+++ Deadlock/src/com/danjerous/Main.java
@@ -0,0 +1,53 @@
+package com.danjerous;
+
+public class Main {
+
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        new Thread1().start();
+        new Thread2().start();
+    }
+
+
+    private static class Thread1 extends Thread {
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("Thread 1: has lock1.");
+                try {
+                    Thread.sleep(100);
+                }catch (InterruptedException e) {
+                    e.getMessage();
+                }
+                System.out.println("Thread 1: Waiting for lock2.");
+                synchronized (lock2) {
+                    System.out.println("Thread 1: has lock1 and lock2.");
+                }
+                System.out.println("Thread 1: Release lock2.");
+            }
+            System.out.println("Thread 1: Released lock1. Exiting.");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        public void run() {
+            synchronized (lock2) {
+                System.out.println("Thread 2: has lock2.");
+
+                try {
+                    Thread.sleep(100);
+                }catch (InterruptedException e) {
+
+                }
+                System.out.println("Thread 2: waiting for lock1.");
+                synchronized (lock1) {
+                    System.out.println("Thread 2: has lock2 and lock1.");
+                }
+                System.out.println("Thread 2 released lock1.");
+            }
+            System.out.println("Thread 2: Released lock2. Exiting.");
+        }
+    }
+
+}
