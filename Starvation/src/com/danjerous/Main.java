f8f7ac14b6c356bb638629dc53c2d7875bfe448c
DanierJ <danierjavid@gmail.com>
2019-06-29 16:30:13 -0500

    Solution to starvation: fair locks

--- Starvation/src/com/danjerous/Main.java
+++ Starvation/src/com/danjerous/Main.java
@@ -1,8 +1,10 @@
 package com.danjerous;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true); // fairness
 
     public static void main(String[] args) {
 
@@ -11,11 +13,7 @@ public class Main {
         Thread t3 = new Thread(new Worker(ThreadColor.ANSI_YELLOW), "Priority 6");
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 2");
-        t1.setPriority(10);
-        t2.setPriority(8);
-        t3.setPriority(6);
-        t4.setPriority(4);
-        t5.setPriority(2);
+
 
         t1.start();
         t2.start();
@@ -36,10 +34,15 @@ public class Main {
         @Override
         public void run() {
             for (int i = 0; i < 100; i++) {
-                synchronized (lock) {
+               lock.lock();
+                try {
+
                     System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
                     // Execute critical section of code.
+                } finally {
+                    lock.unlock();
                 }
+
             }
         }
     }
