74ddf8e496264e45bf269e127dce5811ed099666
DanierJ <danierjavid@gmail.com>
2019-07-01 16:11:08 -0500

    More lambdas

--- Lambdas/src/com/danjerous/Main.java
+++ Lambdas/src/com/danjerous/Main.java
@@ -49,7 +49,7 @@ public class Main {
 
         /// With lambdas
 
-        Collections.sort(employees, (Employee employee1, Employee employee2)->employee1.getName().compareTo(employee2.getName()));
+        Collections.sort(employees, (employee1, employee2)->employee1.getName().compareTo(employee2.getName()));
 
 
         System.out.println("\n");
@@ -57,6 +57,26 @@ public class Main {
             System.out.println(employee.getName());
         }
 
+       /* String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        }, employees.get(0).getName(), employees.get(1).getName());*/
+
+       /*UpperConcat uc = (String s1, String s2) -> s1.toUpperCase() + s2.toUpperCase();*/
+
+       UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+
+       String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+
+        System.out.println(sillyString);
+
+
+    }
+
+    public final static String doStringStuff (UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -91,4 +111,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat (String s1, String s2);
 }
