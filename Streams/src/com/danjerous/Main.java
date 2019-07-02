007b4db5cb4e36b60c01c5626fd0ea4ff32c351d
DanierJ <danierjavid@gmail.com>
2019-07-02 17:14:39 -0500

    Streams - flatMap, accumulator, reducer

--- Streams/src/com/danjerous/Main.java
+++ Streams/src/com/danjerous/Main.java
@@ -3,6 +3,8 @@ package com.danjerous;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -50,6 +52,69 @@ public class Main {
                 .peek(System.out::println)
                 .count()
         );
+
+
+
+        Employee sara = new Employee("Sara Nielsen", 25);
+        Employee keanu = new Employee("Keanu Reeves", 40);
+        Employee jonas = new Employee("Jonas  Kahnwald", 19);
+        Employee martha = new Employee("Martha Nielsen", 19);
+
+        Department biology = new Department("biollogync");
+
+        biology.addEmployee(sara);
+        biology.addEmployee(keanu);
+        biology.addEmployee(jonas);
+
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(martha);
+
+        List<Department> departments = new ArrayList<>();
+
+        departments.add(biology);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+
+        List<String> sortedGNumbers = someBingoNumbers.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(Collectors.toList()); // To save the stream.
+
+List<String> sortedGNumbersArrays = someBingoNumbers.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll); // accumulator.
+
+        sortedGNumbers.stream()
+                .forEach(System.out::println);
+
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+        // Reduce
+
+        // Youngest employee
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+
+        Stream.of("A", "B", "C")
+                .filter(s -> {
+                    System.out.println(s);
+                    return s.startsWith("A");
+                }); // This is lazily evaluated, that means if there's no a terminal operator it won't do a thing.
+
+          
+
 
 
 
