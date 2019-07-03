99b38b9c7316e36b7a3c0eab6f5a3e4553bfa7cb
DanierJ <danierjavid@gmail.com>
2019-07-03 17:24:56 -0500

    SetUp method for every test

--- Banking/src/com/danjerous/BankAccountTest.java
+++ Banking/src/com/danjerous/BankAccountTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
     @org.junit.Test
     public void deposit() {
        // fail("This is test has yet to be implemented.");
-
-        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
-
         assertEquals(1200.00, balance, 0);
 
     }
@@ -23,21 +28,18 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true () {
-        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
         assertTrue("The account is NOT a checking account.", account.isChecking());
     }
 
