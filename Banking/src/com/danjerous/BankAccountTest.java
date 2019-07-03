5ea6ed2e340b05e3c1c40935856300fdd1c68deb
DanierJ <danierjavid@gmail.com>
2019-07-03 16:42:43 -0500

    41. Starting with JUnit

---
+++ Banking/src/com/danjerous/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.danjerous;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        fail("This is test has yet to be implemented.");
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+       fail("This is test has yet to be implemented.");
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("This is test has yet to be implemented.");
+    }
+}
---
+++ Banking/src/com/danjerous/Main.java
@@ -0,0 +1,12 @@
+package com.danjerous;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// First we need to add junit library to our project
+
+        // You just need to ubicarte en la clase que quieres hacer un test unit, le das alt enter y luego crear test
+
+        System.out.println("The app is running.");
+    }
+}
