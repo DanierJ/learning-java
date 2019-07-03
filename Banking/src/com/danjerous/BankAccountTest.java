82caf7a87f70f34890f8a05850b95027b814f357
DanierJ <danierjavid@gmail.com>
2019-07-03 17:13:54 -0500

    Asserts

--- Banking/src/com/danjerous/BankAccountTest.java
+++ Banking/src/com/danjerous/BankAccountTest.java
@@ -6,16 +6,45 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        fail("This is test has yet to be implemented.");
+       // fail("This is test has yet to be implemented.");
+
+        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+
+        assertEquals(1200.00, balance, 0);
+
     }
 
     @org.junit.Test
     public void withdraw() {
        fail("This is test has yet to be implemented.");
+
     }
 
     @org.junit.Test
-    public void getBalance() {
-        fail("This is test has yet to be implemented.");
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
+
+    @org.junit.Test
+    public void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true () {
+        BankAccount account = new BankAccount("Dan", "Jav", 1000.00, BankAccount.CHECKING);
+        assertTrue("The account is NOT a checking account.", account.isChecking());
+    }
+
+
+    /*@org.junit.Test
+    public void dummyTest() {
+        assertEquals(20, 20);
+    }*/
+
 }
