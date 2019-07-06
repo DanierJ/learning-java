cf040b3226e3913f24583845672550f564eea7c6
DanierJ <danierjavid@gmail.com>
2019-07-06 16:40:31 -0500

    Writing code for querying Artists data

--- Music/src/com/danjerous/Main.java
+++ Music/src/com/danjerous/Main.java
@@ -1,8 +1,10 @@
 package com.danjerous;
 
+import com.danjerous.model.Artist;
 import com.danjerous.model.Datasource;
 
 import java.beans.Statement;
+import java.util.List;
 
 public class Main {
 
@@ -12,6 +14,14 @@ public class Main {
         if (!datasource.open()) {
             System.out.println("Can't open datasource.");
             return;
+        }
+
+        List<Artist> artists = datasource.queryArtist();
+
+        if (artists != null) {
+            artists.forEach(artist -> System.out.println("ID: " + artist.getId() + " | name: " + artist.getName()));
+        } else {
+            System.out.println("No artists.");
         }
 
 
--- Music/src/com/danjerous/model/Datasource.java
+++ Music/src/com/danjerous/model/Datasource.java
@@ -1,8 +1,8 @@
 package com.danjerous.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
@@ -44,7 +44,25 @@ public class Datasource {
         }
     }
 
+    public List<Artist> queryArtist() {
 
+        try (Statement statement = conn.createStatement();
+             ResultSet results = statement.executeQuery("SELECT  * FROM " + TABLE_ARTISTS)){
 
+            List<Artist> artists = new ArrayList<>();
 
+            while (results.next()) {
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTIST_ID));
+                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artists.add(artist);
+            }
+
+            return artists;
+
+        } catch (SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
+        }
+    }
 }
