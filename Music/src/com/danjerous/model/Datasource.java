7d5dce3889ae3825b9902a3b08f0111436c69918
DanierJ <danierjavid@gmail.com>
2019-07-06 16:46:47 -0500

    Working with indexes, more efficient

--- Music/src/com/danjerous/model/Datasource.java
+++ Music/src/com/danjerous/model/Datasource.java
@@ -12,14 +12,28 @@ public class Datasource {
     public static final String COLUMN_ALBUMN_NAME = "name";
     public static final String COLUMN_ALBUM_ARTIST = "artist";
 
+    public static final int INDEX_ALBUM_ID = 1;
+    public static final int INDEX_NAME_ARTIST = 2;
+    public static final int INDEX_ALBUM_ARTIST = 3;
+
     public static final String TABLE_ARTISTS = "artists";
     public static final String COLUMN_ARTIST_ID = "_id";
     public static final String COLUMN_ARTIST_NAME = "name";
+
+
+    public static final int INDEX_ARTIST_ID = 1;
+    public static final int INDEX_ARTIST_NAME = 2;
 
     public static final String TABLE_SONGS = "songs";
     public static final String COLUMN_SONG_TRACK = "track";
     public static final String COLUMN_SONG_TITLE = "title";
     public static final String COLUMN_SONG_ALBUM = "album";
+
+
+    public static final int INDEX_SONG_ID = 1;
+    public static final int INDEX_SONG_TRACK = 2;
+    public static final int INDEX_SONG_TITLE = 3;
+    public static final int INDEX_SONG_ALBUM = 4;
 
     private Connection conn;
 
@@ -53,8 +67,8 @@ public class Datasource {
 
             while (results.next()) {
                 Artist artist = new Artist();
-                artist.setId(results.getInt(COLUMN_ARTIST_ID));
-                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artist.setId(results.getInt(INDEX_ARTIST_ID));
+                artist.setName(results.getString(INDEX_ARTIST_NAME));
                 artists.add(artist);
             }
 
